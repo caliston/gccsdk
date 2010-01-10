3e8fcc1dbfe987545e2a7c6a1c7c6131dcf8585d
joty <joty>
2010-01-10 00:22:28 +0000

    Sync with trunk r4431
    git-svn-id: svn://svn.riscos.info/gccsdk/branches/developers@4433 5d7976f4-0b0e-0410-a586-d2bac8e3be52

--- joty/binutils-gcc-separation/recipe/files/gcc/libunixlib/pwd/getpwnam_r.c
+++ joty/binutils-gcc-separation/recipe/files/gcc/libunixlib/pwd/getpwnam_r.c
@@ -1,7 +1,7 @@
 /* getpwnam_r ()
  * Search for an entry with a matching username (re-entrant version).
  * Written by Nick Burrett, 10 December 1997.
- * Copyright (c) 1997-2008 UnixLib Developers
+ * Copyright (c) 1997-2010 UnixLib Developers
  */
 
 #include <stddef.h>
@@ -16,7 +16,7 @@
 /* Search for an entry with a matching name.  */
 int
 getpwnam_r (const char *name, struct passwd *result_buf, char *buffer,
-	    size_t buflen, struct passwd **result)
+            size_t buflen, struct passwd **result)
 {
   FILE *stream;
   struct passwd *p;
@@ -27,27 +27,32 @@ getpwnam_r (const char *name, struct passwd *result_buf, char *buffer,
     return __set_errno (EINVAL);
 
   stream = fopen ("/etc/passwd", "r");
+
   if (stream == NULL)
     {
-      *result = __pwddefault ();
-      return 0;
-    }
+      p = __pwddefault ();
 
-  p = __pwdread (stream, result_buf, buffer, buflen);
-  while (p != NULL)
+      if (result_buf)
+	memcpy (result_buf, p, sizeof(struct passwd));
+    }
+  else
     {
-      if (strcmp (result_buf->pw_name, name) == 0)
-        break;
       p = __pwdread (stream, result_buf, buffer, buflen);
-    }
+      while (p != NULL)
+        {
+          if (strcmp (result_buf->pw_name, name) == 0)
+            break;
+          p = __pwdread (stream, result_buf, buffer, buflen);
+        }
 
-  fclose (stream);
+      fclose (stream);
+   }
 
   if (p == NULL)
     return -1;
-
   if (result != NULL)
     *result = result_buf;
+  strncpy (buffer, result ? (*result)->pw_passwd : p->pw_passwd, buflen);
 
   return 0;
 }
