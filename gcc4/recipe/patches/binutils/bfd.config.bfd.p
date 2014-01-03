caliston/gccsdk

commit 1ec28d8ad4beb21642836af1cb8f4ca7bfe6f4b1

Use binutils 2.21 instead of binutils 2.20.1
git-svn-id: svn://svn.riscos.info/gccsdk/trunk@4911 5d7976f4-0b0e-0410-a586-d2bac8e3be52

gcc4/Makefile
@@ -4,8 +4,8 @@
 # Needed:
 #   - apt-get install flex bison libgmp3-dev libmpfr-dev texinfo build-essential
 #   - binutils 2.17 & gcc 4.1 need automake 1.9.6 and autoconf 2.59.
-#     binutils 2.20.1 needs automake 1.11.1, while gcc 4.4 needs automake 1.11.
-#     Both binutils 2.20.1 and gcc 4.4 need autoconf 2.64.
+#     binutils 2.21 needs automake 1.11.1, while gcc 4.4 needs automake 1.11.
+#     Both binutils 2.21 and gcc 4.4 need autoconf 2.64.
 #     Note the required automake/autoconf sources will be fetched & built automatically.
 #   - gdb requires libncurses5-dev
 
@@ -52,9 +52,9 @@ GCC_CONFIG_ARGS := --disable-threads --disable-multilib --disable-shared --with-
 BINUTILS_CONFIG_ARGS := --disable-multilib --disable-shared
 endif
 # Note: --with-pkgversion & --with-bugurl is only supported from binutils 2.18 (?) & gcc 4.3 onwards.
-GCC_CONFIG_ARGS += --with-pkgversion='GCCSDK GCC $(GCC_VERSION) Release 3 Development' \
+GCC_CONFIG_ARGS += --with-pkgversion='GCCSDK GCC $(GCC_VERSION) Release 1 Development' \
 	--with-bugurl=http://gccsdk.riscos.info/
-BINUTILS_CONFIG_ARGS += --with-pkgversion='GCCSDK GCC $(GCC_VERSION) Release 3 Development' \
+BINUTILS_CONFIG_ARGS += --with-pkgversion='GCCSDK GCC $(GCC_VERSION) Release 1 Development' \
 	--with-bugurl=http://gccsdk.riscos.info/
 BINUTILS_CONFIG_ARGS += --enable-maintainer-mode --enable-interwork --disable-werror --with-gcc --disable-nls
 GCC_CONFIG_ARGS += --enable-maintainer-mode --enable-interwork --disable-nls

gcc4/README
@@ -2,7 +2,7 @@ The RISC OS Software Development Kit [GCCSDK GCC 4.1.2 Release 1 - Development]
 ====================================
 
 This is a lightweight build environment which currently combines GCC 4.1.2
-release, binutils 2.20.1 release, UnixLib 5.0 and libscl 2.0 into one directory
+release, binutils 2.21 release, UnixLib 5.0 and libscl 2.0 into one directory
 which can be used to build a RISC OS cross-compiler and native RISC OS compiler
 with target 'arm-unknown-riscos' (APCS-32 compliant).
 

gcc4/recipe/patches/binutils/bfd.config.bfd.p
@@ -1,6 +1,6 @@
---- bfd/config.bfd.orig	2009-08-06 19:38:00.000000000 +0200
-+++ bfd/config.bfd	2010-01-31 16:19:42.035012118 +0100
-@@ -282,7 +282,7 @@ case "${targ}" in
+--- bfd/config.bfd.orig	2010-12-01 12:03:10.000000000 +0100
++++ bfd/config.bfd	2010-12-11 02:00:02.753464116 +0100
+@@ -281,7 +281,7 @@ case "${targ}" in
      ;;
    arm-*-elf | arm-*-freebsd* | arm*-*-linux-* | arm*-*-conix* | \
    arm*-*-uclinux* | arm-*-kfreebsd*-gnu | \

gcc4/recipe/patches/binutils/bfd.elf.c.p
@@ -1,6 +1,6 @@
---- bfd/elf.c.orig	2009-09-10 13:47:12.000000000 +0200
-+++ bfd/elf.c	2010-01-31 16:31:08.065017338 +0100
-@@ -3759,9 +3759,18 @@ _bfd_elf_map_sections_to_segments (bfd *
+--- bfd/elf.c.orig	2010-10-29 14:10:24.000000000 +0200
++++ bfd/elf.c	2010-12-11 02:08:00.814996811 +0100
+@@ -3892,8 +3892,17 @@ _bfd_elf_map_sections_to_segments (bfd *
  	    }
  	  else if (! writable
  		   && (hdr->flags & SEC_READONLY) == 0
@@ -10,9 +10,8 @@
 +	   merging them into one. This change forces LD to maintain separate
 +	   segments. Perhaps this functionality should be offered as a command
 +	   line option. */
- 		   && (((last_hdr->lma + last_size - 1)
- 			& ~(maxpagesize - 1))
- 		       != (hdr->lma & ~(maxpagesize - 1))))
+ 		   && (((last_hdr->lma + last_size - 1) & -maxpagesize)
+ 		       != (hdr->lma & -maxpagesize)))
 +#else
 +		  )
 +#endif

commit 14145283396e3df61cf72972b03da1c7df89decf

- Upgrade binutils 2.23.2 to 2.24. - recipe/patches/binutils/ld.scripttempl.elf.sc.p: Fix bug introduced in r4911 : .${BSS_NAME}* were added twice.
git-svn-id: svn://svn.riscos.info/gccsdk/trunk@6538 5d7976f4-0b0e-0410-a586-d2bac8e3be52

gcc4/Makefile
@@ -4,7 +4,7 @@
 #
 # Build requirements:
 #   - apt-get install flex bison libgmp3-dev libmpfr-dev texinfo build-essential
-#   - binutils 2.23.2 and gcc 4.7 needs automake 1.11.1 and autoconf 2.64 and
+#   - binutils 2.24 and gcc 4.7 needs automake 1.11.1 and autoconf 2.64 and
 #     a recent libtool version.
 #     Note the required automake/autoconf/libtool sources will be fetched
 #     & built automatically.

gcc4/README
@@ -1,7 +1,7 @@
 The RISC OS Software Development Kit [GCCSDK GCC 4.7.4 Release 1 - Development]
 
 This is a lightweight build environment which currently combines GCC 4.7.4
-release, binutils 2.23.2 release, UnixLib 5.0 into one directory which can be
+release, binutils 2.24 release, UnixLib 5.0 into one directory which can be
 used to build a RISC OS cross-compiler and native RISC OS compiler with
 target 'arm-unknown-riscos' (APCS-32 compliant).
 
@@ -431,5 +431,5 @@ important contributions from many people in the original GCC 3.x GCCSDK;
 in particular, endless improvements to UnixLib which made it practical to
 run a wide range of software on RISC OS.  See those versions for full details.
 
-Copyright (c) 2005-2010 GCCSDK Developers
+Copyright (c) 2005-2014 GCCSDK Developers
 <URL:http://gccsdk.riscos.info/>

gcc4/recipe/patches/binutils/bfd.config.bfd.p
@@ -1,6 +1,6 @@
---- bfd/config.bfd.orig	2012-09-04 16:14:59.000000000 +0200
-+++ bfd/config.bfd	2012-11-17 14:55:35.729616193 +0100
-@@ -315,7 +315,7 @@ case "${targ}" in
+--- bfd/config.bfd.orig	2013-11-04 16:33:37.000000000 +0100
++++ bfd/config.bfd	2013-12-31 13:16:36.386127574 +0100
+@@ -333,7 +333,7 @@ case "${targ}" in
      ;;
    arm-*-elf | arm-*-freebsd* | arm*-*-linux-* | arm*-*-conix* | \
    arm*-*-uclinux* | arm-*-kfreebsd*-gnu | \

gcc4/recipe/patches/binutils/bfd.elf.c.p
@@ -1,6 +1,6 @@
---- bfd/elf.c.orig	2013-03-25 09:06:19.000000000 +0100
-+++ bfd/elf.c	2013-03-26 23:24:38.309499460 +0100
-@@ -1242,6 +1242,7 @@ _bfd_elf_print_private_bfd_data (bfd *ab
+--- bfd/elf.c.orig	2013-11-04 16:33:37.000000000 +0100
++++ bfd/elf.c	2013-12-31 13:16:36.446126819 +0100
+@@ -1251,6 +1251,7 @@ _bfd_elf_print_private_bfd_data (bfd *ab
  		}
  	      break;
  
@@ -8,7 +8,7 @@
  	    case DT_NEEDED: name = "NEEDED"; stringp = TRUE; break;
  	    case DT_PLTRELSZ: name = "PLTRELSZ"; break;
  	    case DT_PLTGOT: name = "PLTGOT"; break;
-@@ -3941,8 +3942,17 @@ _bfd_elf_map_sections_to_segments (bfd *
+@@ -3951,8 +3952,17 @@ _bfd_elf_map_sections_to_segments (bfd *
  	    }
  	  else if (! writable
  		   && (hdr->flags & SEC_READONLY) == 0

gcc4/recipe/patches/binutils/bfd.elf32-arm.c.p
@@ -1,6 +1,6 @@
---- bfd/elf32-arm.c.orig	2013-03-25 09:06:19.000000000 +0100
-+++ bfd/elf32-arm.c	2013-03-26 23:24:38.261500065 +0100
-@@ -2080,7 +2080,7 @@ typedef unsigned short int insn16;
+--- bfd/elf32-arm.c.orig	2013-11-08 11:13:48.000000000 +0100
++++ bfd/elf32-arm.c	2013-12-31 13:54:42.841382938 +0100
+@@ -2079,7 +2079,7 @@ typedef unsigned short int insn16;
  
  /* The name of the dynamic interpreter.  This is put in the .interp
     section.  */
@@ -9,7 +9,7 @@
  
  static const unsigned long tls_trampoline [] =
  {
-@@ -2239,6 +2239,56 @@ static const bfd_vma elf32_arm_nacl_plt_
+@@ -2238,6 +2238,56 @@ static const bfd_vma elf32_arm_nacl_plt_
    0xea000000,		/* b	.Lplt_tail			*/
  };
  
@@ -66,7 +66,7 @@
  #define ARM_MAX_FWD_BRANCH_OFFSET  ((((1 << 23) - 1) << 2) + 8)
  #define ARM_MAX_BWD_BRANCH_OFFSET  ((-((1 << 23) << 2)) + 8)
  #define THM_MAX_FWD_BRANCH_OFFSET  ((1 << 22) -2 + 4)
-@@ -2998,6 +3048,26 @@ struct elf32_arm_link_hash_table
+@@ -3026,6 +3076,26 @@ struct elf32_arm_link_hash_table
    unsigned int bfd_count;
    int top_index;
    asection **input_list;
@@ -93,7 +93,7 @@
  };
  
  /* Create an entry in an ARM ELF linker hash table.  */
-@@ -3289,6 +3359,63 @@ create_ifunc_sections (struct bfd_link_i
+@@ -3317,6 +3387,63 @@ create_ifunc_sections (struct bfd_link_i
    return TRUE;
  }
  
@@ -157,7 +157,7 @@
  /* Create .plt, .rel(a).plt, .got, .got.plt, .rel(a).got, .dynbss, and
     .rel(a).bss sections in DYNOBJ, and set up shortcuts to them in our
     hash table.  */
-@@ -3333,6 +3460,20 @@ elf32_arm_create_dynamic_sections (bfd *
+@@ -3361,6 +3488,20 @@ elf32_arm_create_dynamic_sections (bfd *
  	}
      }
  
@@ -178,7 +178,7 @@
    if (!htab->root.splt
        || !htab->root.srelplt
        || !htab->sdynbss
-@@ -3483,9 +3624,209 @@ elf32_arm_link_hash_table_create (bfd *a
+@@ -3476,9 +3617,209 @@ elf32_arm_link_hash_table_create (bfd *a
        return NULL;
      }
  
@@ -388,7 +388,7 @@
  /* Free the derived linker hash table.  */
  
  static void
-@@ -7690,6 +8031,56 @@ elf32_arm_populate_plt_entry (bfd *outpu
+@@ -7703,6 +8044,56 @@ elf32_arm_populate_plt_entry (bfd *outpu
  			      elf32_arm_plt_thumb_stub[1], ptr - 2);
  	    }
  
@@ -445,7 +445,7 @@
  	  put_arm_insn (htab, output_bfd,
  			elf32_arm_plt_entry[0]
  			| ((got_displacement & 0x0ff00000) >> 20),
-@@ -7705,6 +8096,7 @@ elf32_arm_populate_plt_entry (bfd *outpu
+@@ -7718,6 +8109,7 @@ elf32_arm_populate_plt_entry (bfd *outpu
  #ifdef FOUR_WORD_PLT
  	  bfd_put_32 (output_bfd, elf32_arm_plt_entry[3], ptr + 12);
  #endif
@@ -453,7 +453,7 @@
  	}
  
        /* Fill in the entry in the .rel(a).(i)plt section.  */
-@@ -8110,6 +8502,67 @@ elf32_arm_final_link_relocate (reloc_how
+@@ -8128,6 +8520,67 @@ elf32_arm_final_link_relocate (reloc_how
      }
    else
      addend = signed_addend = rel->r_addend;
@@ -519,17 +519,17 @@
 +	}
 +    }
  
-   /* Record the symbol information that should be used in dynamic
-      relocations.  */
-@@ -9117,6 +9570,7 @@ elf32_arm_final_link_relocate (reloc_how
-          define _GLOBAL_OFFSET_TABLE in a different way, as is
-          permitted by the ABI, we might have to change this
-          calculation.  */
+   /* ST_BRANCH_TO_ARM is nonsense to thumb-only targets when we
+      are resolving a function call relocation.  */
+@@ -9144,6 +9597,7 @@ elf32_arm_final_link_relocate (reloc_how
+ 	 define _GLOBAL_OFFSET_TABLE in a different way, as is
+ 	 permitted by the ABI, we might have to change this
+ 	 calculation.  */
 +      if (globals->s_ro_module_reloccode == NULL)
        value -= sgot->output_section->vma;
        return _bfd_final_link_relocate (howto, input_bfd, input_section,
  				       contents, rel->r_offset, value,
-@@ -10912,10 +11366,89 @@ elf32_arm_final_link (bfd *abfd, struct
+@@ -10942,10 +11396,89 @@ elf32_arm_final_link (bfd *abfd, struct
  {
    struct elf32_arm_link_hash_table *globals = elf32_arm_hash_table (info);
    asection *sec, *osec;
@@ -619,7 +619,7 @@
    /* Invoke the regular ELF backend linker to do all the work.  */
    if (!bfd_elf_final_link (abfd, info))
      return FALSE;
-@@ -10962,6 +11495,63 @@ elf32_arm_final_link (bfd *abfd, struct
+@@ -10992,6 +11525,63 @@ elf32_arm_final_link (bfd *abfd, struct
  	return FALSE;
      }
  
@@ -683,7 +683,7 @@
    return TRUE;
  }
  
-@@ -12479,6 +13069,22 @@ elf32_arm_check_relocs (bfd *abfd, struc
+@@ -12536,6 +13126,22 @@ elf32_arm_check_relocs (bfd *abfd, struc
  
        eh = (struct elf32_arm_link_hash_entry *) h;
  
@@ -706,7 +706,7 @@
        call_reloc_p = FALSE;
        may_become_dynamic_p = FALSE;
        may_need_local_target_p = FALSE;
-@@ -13146,6 +13752,8 @@ allocate_dynrelocs_for_symbol (struct el
+@@ -13203,6 +13809,8 @@ allocate_dynrelocs_for_symbol (struct el
  	{
  	  elf32_arm_allocate_plt_entry (info, eh->is_iplt, &h->plt, &eh->plt);
  
@@ -715,7 +715,7 @@
  	  /* If this symbol is not defined in a regular file, and we are
  	     not generating a shared library, then set the symbol to this
  	     location in the .plt.  This is required to make function
-@@ -13830,6 +14438,22 @@ elf32_arm_size_dynamic_sections (bfd * o
+@@ -13892,6 +14500,22 @@ elf32_arm_size_dynamic_sections (bfd * o
  #define add_dynamic_entry(TAG, VAL) \
    _bfd_elf_add_dynamic_entry (info, TAG, VAL)
  
@@ -738,7 +738,7 @@
       if (info->executable)
  	{
  	  if (!add_dynamic_entry (DT_DEBUG, 0))
-@@ -13838,8 +14462,7 @@ elf32_arm_size_dynamic_sections (bfd * o
+@@ -13900,8 +14524,7 @@ elf32_arm_size_dynamic_sections (bfd * o
  
        if (plt)
  	{
@@ -748,7 +748,7 @@
  	      || !add_dynamic_entry (DT_PLTREL,
  				     htab->use_rel ? DT_REL : DT_RELA)
  	      || !add_dynamic_entry (DT_JMPREL, 0))
-@@ -13869,6 +14492,9 @@ elf32_arm_size_dynamic_sections (bfd * o
+@@ -13931,6 +14554,9 @@ elf32_arm_size_dynamic_sections (bfd * o
  	    }
  	}
  
@@ -758,7 +758,7 @@
        /* If any dynamic relocs apply to a read-only section,
  	 then we need a DT_TEXTREL entry.  */
        if ((info->flags & DF_TEXTREL) == 0)
-@@ -14086,7 +14712,12 @@ elf32_arm_finish_dynamic_sections (bfd *
+@@ -14180,7 +14806,12 @@ elf32_arm_finish_dynamic_sections (bfd *
  		  && elf_vxworks_finish_dynamic_entry (output_bfd, &dyn))
  		bfd_elf32_swap_dyn_out (output_bfd, &dyn, dyncon);
  	      break;
@@ -772,7 +772,7 @@
  	    case DT_HASH:
  	      name = ".hash";
  	      goto get_vma_if_bpabi;
-@@ -14294,6 +14925,39 @@ elf32_arm_finish_dynamic_sections (bfd *
+@@ -14372,6 +15003,39 @@ elf32_arm_finish_dynamic_sections (bfd *
  	    {
  	      got_displacement = got_address - (plt_address + 16);
  
@@ -812,7 +812,7 @@
  	      plt0_entry = elf32_arm_plt0_entry;
  	      put_arm_insn (htab, output_bfd, plt0_entry[0],
  			    splt->contents + 0);
-@@ -14313,6 +14977,7 @@ elf32_arm_finish_dynamic_sections (bfd *
+@@ -14391,6 +15055,7 @@ elf32_arm_finish_dynamic_sections (bfd *
  #endif
  	    }
  	}
@@ -820,7 +820,7 @@
  
        /* UnixWare sets the entsize of .plt to 4, although that doesn't
  	 really seem like the right value.  */
-@@ -15600,13 +16265,28 @@ elf32_arm_add_symbol_hook (bfd *abfd, st
+@@ -15703,13 +16368,28 @@ elf32_arm_add_symbol_hook (bfd *abfd, st
  				       flagsp, secp, valp))
      return FALSE;
  

gcc4/recipe/patches/binutils/bfd.opncls.c.p
@@ -1,6 +1,6 @@
---- bfd/opncls.c.orig	2013-03-25 09:06:20.000000000 +0100
-+++ bfd/opncls.c	2013-03-26 23:24:38.321499310 +0100
-@@ -28,6 +28,12 @@
+--- bfd/opncls.c.orig	2013-11-08 11:13:48.000000000 +0100
++++ bfd/opncls.c	2013-12-31 13:16:36.450126769 +0100
+@@ -26,6 +26,12 @@
  #include "libbfd.h"
  #include "libiberty.h"
  
@@ -13,7 +13,7 @@
  #ifndef S_IXUSR
  #define S_IXUSR 0100	/* Execute by owner.  */
  #endif
-@@ -678,6 +684,23 @@ _maybe_make_executable (bfd * abfd)
+@@ -679,6 +685,23 @@ _maybe_make_executable (bfd * abfd)
  	  chmod (abfd->filename,
  		 (0777
  		  & (buf.st_mode | ((S_IXUSR | S_IXGRP | S_IXOTH) &~ mask))));

gcc4/recipe/patches/binutils/binutils.readelf.c.p
@@ -1,6 +1,6 @@
---- binutils/readelf.c.orig	2012-11-05 17:27:35.000000000 +0100
-+++ binutils/readelf.c	2012-11-17 14:55:35.865614483 +0100
-@@ -1798,6 +1798,12 @@ get_dynamic_type (unsigned long type)
+--- binutils/readelf.c.orig	2013-11-18 09:40:15.000000000 +0100
++++ binutils/readelf.c	2013-12-31 13:16:36.454126719 +0100
+@@ -1865,6 +1865,12 @@ get_dynamic_type (unsigned long type)
  	    case EM_IA_64:
  	      result = get_ia64_dynamic_type (type);
  	      break;

gcc4/recipe/patches/binutils/gas.config.tc-arm.c.p
@@ -1,8 +1,8 @@
---- gas/config/tc-arm.c.orig	2013-06-15 14:28:31.000000000 +0100
-+++ gas/config/tc-arm.c	2013-06-15 14:17:06.000000000 +0100
-@@ -241,7 +241,10 @@ static const arm_feature_set fpu_neon_ex
- static const arm_feature_set fpu_crypto_ext_armv8 =
-   ARM_FEATURE (0, FPU_CRYPTO_EXT_ARMV8);
+--- gas/config/tc-arm.c.orig	2013-11-26 12:37:33.000000000 +0100
++++ gas/config/tc-arm.c	2013-12-31 13:51:51.279539763 +0100
+@@ -241,7 +241,10 @@ static const arm_feature_set fpu_crypto_
+ static const arm_feature_set crc_ext_armv8 =
+   ARM_FEATURE (0, CRC_EXT_ARMV8);
  
 -static int mfloat_abi_opt = -1;
 +#ifndef DEFAULT_FLOAT_ABI
@@ -12,32 +12,13 @@
  /* Record user cpu selection for object attributes.  */
  static arm_feature_set selected_cpu = ARM_ARCH_NONE;
  /* Must be long enough to hold any of the names in arm_cpus.  */
-@@ -885,6 +888,9 @@ const char FLT_CHARS[] = "rRsSfFdDxXeEpP
- static inline int
- skip_past_char (char ** str, char c)
- {
-+  /* PR gas/14987: Allow for whitespace before the expected character.  */
-+  skip_whitespace (*str);
-+
-   if (**str == c)
-     {
-       (*str)++;
-@@ -5168,6 +5174,9 @@ parse_address_main (char **str, int i, i
-       return PARSE_OPERAND_SUCCESS;
-     }
- 
-+  /* PR gas/14887: Allow for whitespace after the opening bracket.  */
-+  skip_whitespace (p);
-+
-   if ((reg = arm_reg_parse (&p, REG_TYPE_RN)) == FAIL)
-     {
-       inst.error = _(reg_expected_msgs[REG_TYPE_RN]);
-@@ -22672,6 +22681,12 @@ tc_gen_reloc (asection *section, fixS *f
+@@ -22771,6 +22774,13 @@ tc_gen_reloc (asection *section, fixS *f
  	}
  
      case BFD_RELOC_32:
++      /* RISC OS */
 +      if (streq (S_GET_NAME (fixp->fx_addsy), "__GOTT_BASE__"))
-+        {
++	{
 +	  code = BFD_RELOC_NONE;
 +	  break;
 +	}
@@ -45,7 +26,7 @@
        if (fixp->fx_pcrel)
  	{
  	  code = BFD_RELOC_32_PCREL;
-@@ -22814,6 +22829,13 @@ tc_gen_reloc (asection *section, fixS *f
+@@ -22913,6 +22923,13 @@ tc_gen_reloc (asection *section, fixS *f
        return NULL;
  
      case BFD_RELOC_ARM_OFFSET_IMM:
@@ -59,17 +40,18 @@
        if (section->use_rela_p)
  	{
  	  code = fixp->fx_r_type;
-@@ -25096,3 +25118,13 @@ arm_apply_sym_value (struct fix * fixP)
+@@ -25204,3 +25221,14 @@ arm_apply_sym_value (struct fix * fixP)
    return 0;
  }
  #endif /* OBJ_ELF */
 +
-+void riscos_symbol_new_hook (symbolS *sym)
++void
++riscos_symbol_new_hook (symbolS *sym)
 +{
 +  const char *sym_name = S_GET_NAME (sym);
 +
 +  if (sym_name[0] == '_' && sym_name[1] == '_'
-+   && (strcmp (sym_name, "__GOTT_BASE__") == 0
-+    || strcmp (sym_name, "__GOTT_INDEX__") == 0))
++      && (strcmp (sym_name, "__GOTT_BASE__") == 0
++	  || strcmp (sym_name, "__GOTT_INDEX__") == 0))
 +    S_SET_WEAK (sym);
 +}

gcc4/recipe/patches/binutils/gas.configure.tgt.p
@@ -1,6 +1,6 @@
---- gas/configure.tgt.orig	2012-09-04 14:53:45.000000000 +0200
-+++ gas/configure.tgt	2012-11-17 14:55:35.909613930 +0100
-@@ -132,6 +132,7 @@ case ${generic_target} in
+--- gas/configure.tgt.orig	2013-11-04 16:33:37.000000000 +0100
++++ gas/configure.tgt	2013-12-31 13:16:36.478126417 +0100
+@@ -150,6 +150,7 @@ case ${generic_target} in
  					fmt=coff em=wince-pe ;;
    arm-*-pe)				fmt=coff em=pe ;;
    arm-*-riscix*)			fmt=aout em=riscix ;;

gcc4/recipe/patches/binutils/include.bfdlink.h.p
@@ -1,6 +1,6 @@
---- include/bfdlink.h.orig	2012-04-09 18:27:18.000000000 +0200
-+++ include/bfdlink.h	2012-11-17 14:55:35.925613728 +0100
-@@ -501,6 +501,9 @@ struct bfd_link_info
+--- include/bfdlink.h.orig	2013-11-04 16:33:39.000000000 +0100
++++ include/bfdlink.h	2013-12-31 13:16:36.478126417 +0100
+@@ -524,6 +524,9 @@ struct bfd_link_info
    /* Start and end of RELRO region.  */
    bfd_vma relro_start, relro_end;
  

gcc4/recipe/patches/binutils/include.elf.common.h.p
@@ -1,6 +1,6 @@
---- include/elf/common.h.orig	2012-09-04 14:53:47.000000000 +0200
-+++ include/elf/common.h	2012-11-17 14:55:35.941613528 +0100
-@@ -736,6 +736,9 @@
+--- include/elf/common.h.orig	2013-11-04 16:33:39.000000000 +0100
++++ include/elf/common.h	2013-12-31 13:16:36.482126367 +0100
+@@ -754,6 +754,9 @@
  #define DT_LOPROC	0x70000000
  #define DT_HIPROC	0x7fffffff
  

gcc4/recipe/patches/binutils/ld.Makefile.am.p
@@ -1,6 +1,6 @@
---- ld/Makefile.am.orig	2012-09-04 14:53:47.000000000 +0200
-+++ ld/Makefile.am	2012-11-17 14:55:35.961613275 +0100
-@@ -145,6 +145,7 @@ ALL_EMULATION_SOURCES = \
+--- ld/Makefile.am.orig	2013-11-26 12:37:33.000000000 +0100
++++ ld/Makefile.am	2013-12-31 13:16:36.490126266 +0100
+@@ -163,6 +163,7 @@ ALL_EMULATION_SOURCES = \
  	earmelf_linux_eabi.c \
  	earmelf_nacl.c \
  	earmelf_nbsd.c \
@@ -8,7 +8,7 @@
  	earmelf_vxworks.c \
  	earmelfb.c \
  	earmelfb_linux.c \
-@@ -710,6 +711,10 @@ earmelf_fbsd.c: $(srcdir)/emulparams/arm
+@@ -734,6 +735,10 @@ earmelf_fbsd.c: $(srcdir)/emulparams/arm
    $(ELF_DEPS) $(srcdir)/emultempl/armelf.em \
    $(srcdir)/scripttempl/elf.sc ${GEN_DEPENDS}
  	${GENSCRIPTS} armelf_fbsd "$(tdir_armelf_fbsd)"

gcc4/recipe/patches/binutils/ld.configure.tgt.p
@@ -1,6 +1,6 @@
---- ld/configure.tgt.orig	2012-09-04 14:53:47.000000000 +0200
-+++ ld/configure.tgt	2012-11-17 14:55:35.945613478 +0100
-@@ -74,6 +74,7 @@ arm-*-nto*)		targ_emul=armnto ;;
+--- ld/configure.tgt.orig	2013-11-26 12:37:33.000000000 +0100
++++ ld/configure.tgt	2013-12-31 13:16:36.482126367 +0100
+@@ -94,6 +94,7 @@ arm-*-nto*)		targ_emul=armnto ;;
  arm-*-openbsd*)		targ_emul=armnbsd ;;
  arm-*-rtems*)		targ_emul=armelf ;;
  armeb-*-elf)		targ_emul=armelfb ;;

gcc4/recipe/patches/binutils/ld.emultempl.armelf.em.p
@@ -1,6 +1,6 @@
---- ld/emultempl/armelf.em.orig	2013-03-25 09:06:23.000000000 +0100
-+++ ld/emultempl/armelf.em	2013-03-26 23:24:38.745493980 +0100
-@@ -43,6 +43,7 @@ static int no_wchar_size_warning = 0;
+--- ld/emultempl/armelf.em.orig	2013-11-04 16:33:39.000000000 +0100
++++ ld/emultempl/armelf.em	2013-12-31 13:16:36.486126316 +0100
+@@ -41,6 +41,7 @@ static int no_wchar_size_warning = 0;
  static int pic_veneer = 0;
  static int merge_exidx_entries = -1;
  static int fix_arm1176 = 1;
@@ -8,7 +8,7 @@
  
  static void
  gld${EMULATION_NAME}_before_parse (void)
-@@ -56,6 +57,28 @@ gld${EMULATION_NAME}_before_parse (void)
+@@ -54,6 +55,28 @@ gld${EMULATION_NAME}_before_parse (void)
  }
  
  static void
@@ -37,7 +37,7 @@
  arm_elf_before_allocation (void)
  {
    bfd_elf32_arm_set_byteswap_code (&link_info, byteswap_code);
-@@ -88,8 +111,30 @@ arm_elf_before_allocation (void)
+@@ -86,8 +109,30 @@ arm_elf_before_allocation (void)
        bfd_elf32_arm_allocate_interworking_sections (& link_info);
      }
  
@@ -68,7 +68,7 @@
  }
  
  /* Fake input file for stubs.  */
-@@ -534,6 +579,9 @@ PARSE_AND_LIST_PROLOGUE='
+@@ -533,6 +578,9 @@ PARSE_AND_LIST_PROLOGUE='
  #define OPTION_NO_MERGE_EXIDX_ENTRIES   316
  #define OPTION_FIX_ARM1176		317
  #define OPTION_NO_FIX_ARM1176		318
@@ -78,7 +78,7 @@
  '
  
  PARSE_AND_LIST_SHORTOPTS=p
-@@ -558,6 +606,9 @@ PARSE_AND_LIST_LONGOPTS='
+@@ -557,6 +605,9 @@ PARSE_AND_LIST_LONGOPTS='
    { "no-merge-exidx-entries", no_argument, NULL, OPTION_NO_MERGE_EXIDX_ENTRIES },
    { "fix-arm1176", no_argument, NULL, OPTION_FIX_ARM1176 },
    { "no-fix-arm1176", no_argument, NULL, OPTION_NO_FIX_ARM1176 },
@@ -88,7 +88,7 @@
  '
  
  PARSE_AND_LIST_OPTIONS='
-@@ -587,6 +638,9 @@ PARSE_AND_LIST_OPTIONS='
+@@ -586,6 +637,9 @@ PARSE_AND_LIST_OPTIONS='
    fprintf (file, _("  --[no-]fix-cortex-a8        Disable/enable Cortex-A8 Thumb-2 branch erratum fix\n"));
    fprintf (file, _("  --no-merge-exidx-entries    Disable merging exidx entries\n"));
    fprintf (file, _("  --[no-]fix-arm1176          Disable/enable ARM1176 BLX immediate erratum fix\n"));
@@ -98,7 +98,7 @@
  '
  
  PARSE_AND_LIST_ARGS_CASES='
-@@ -678,8 +732,23 @@ PARSE_AND_LIST_ARGS_CASES='
+@@ -677,8 +731,23 @@ PARSE_AND_LIST_ARGS_CASES='
     case OPTION_NO_FIX_ARM1176:
        fix_arm1176 = 0;
        break;

gcc4/recipe/patches/binutils/ld.scripttempl.elf.sc.p
@@ -1,6 +1,6 @@
---- ld/scripttempl/elf.sc.orig	2013-03-25 09:06:23.000000000 +0100
-+++ ld/scripttempl/elf.sc	2013-03-26 23:24:38.761493779 +0100
-@@ -316,6 +316,14 @@ else
+--- ld/scripttempl/elf.sc.orig	2013-11-04 16:33:39.000000000 +0100
++++ ld/scripttempl/elf.sc	2013-12-31 13:30:40.247518799 +0100
+@@ -315,6 +315,14 @@ else
     test -z "${TEXT_BASE_ADDRESS}" && TEXT_BASE_ADDRESS="${TEXT_START_ADDR}"
  fi
  
@@ -15,15 +15,15 @@
  cat <<EOF
  OUTPUT_FORMAT("${OUTPUT_FORMAT}", "${BIG_OUTPUT_FORMAT}",
  	      "${LITTLE_OUTPUT_FORMAT}")
-@@ -336,6 +344,7 @@ SECTIONS
+@@ -335,6 +343,7 @@ SECTIONS
    ${CREATE_SHLIB-${CREATE_PIE-${RELOCATING+PROVIDE (__executable_start = ${TEXT_START_ADDR}); . = ${TEXT_BASE_ADDRESS};}}}
    ${CREATE_SHLIB+${RELOCATING+. = ${SHLIB_TEXT_START_ADDR}${SIZEOF_HEADERS_CODE};}}
-   ${CREATE_PIE+${RELOCATING+. = ${SHLIB_TEXT_START_ADDR}${SIZEOF_HEADERS_CODE};}}
+   ${CREATE_PIE+${RELOCATING+PROVIDE (__executable_start = ${SHLIB_TEXT_START_ADDR}); . = ${SHLIB_TEXT_START_ADDR}${SIZEOF_HEADERS_CODE};}}
 +  ${CREATE_SHLIB-${RISCOS_ROBASE}}
  EOF
  
  emit_early_ro()
-@@ -459,6 +468,8 @@ emit_dyn()
+@@ -458,6 +467,8 @@ emit_dyn()
  test -n "${NON_ALLOC_DYN}${SEPARATE_CODE}" || emit_dyn
  
  cat <<EOF
@@ -33,8 +33,8 @@
    {
      ${RELOCATING+${INIT_START}}
 @@ -531,6 +542,11 @@ cat <<EOF
-   .exception_ranges ${RELOCATING-0} : ONLY_IF_RO { *(.exception_ranges
    .exception_ranges*) }
+   ${TEXT_PLT+${PLT_NEXT_DATA+${PLT}}}
  
 +  /* RISC OS PIC relocs */
 +  .riscos.pic : { *(.riscos.pic) }
@@ -44,7 +44,7 @@
    /* Adjust the address for the data segment.  We want to adjust up to
       the same address within the page on the next page up.  */
    ${CREATE_SHLIB-${CREATE_PIE-${RELOCATING+. = ${DATA_ADDR-${DATA_SEGMENT_ALIGN}};}}}
-@@ -570,6 +586,11 @@ cat <<EOF
+@@ -571,6 +587,11 @@ cat <<EOF
  
    ${DATA_PLT+${PLT_BEFORE_GOT-${PLT}}}
  
@@ -56,27 +56,27 @@
    .data         ${RELOCATING-0} :
    {
      ${RELOCATING+${DATA_START_SYMBOLS}}
-@@ -595,8 +616,11 @@ cat <<EOF
+@@ -596,8 +617,10 @@ cat <<EOF
    ${BSS_PLT+${PLT}}
    .${BSS_NAME}          ${RELOCATING-0} :
    {
 +   ${RELOCATING+${CREATE_SHLIB-${RISCOS_ZIBASE}}}
     *(.dyn${BSS_NAME})
-    *(.${BSS_NAME}${RELOCATING+ .${BSS_NAME}.* .gnu.linkonce.b.*})
+-   *(.${BSS_NAME}${RELOCATING+ .${BSS_NAME}.* .gnu.linkonce.b.*})
 +   /* Sort the .${BSS_NAME} sections to avoid runtime check errors in SCL.  */
 +   *(.${BSS_NAME}${RELOCATING+ SORT(.${BSS_NAME}.*) .gnu.linkonce.b.*})
     *(COMMON)
     /* Align here to ensure that the .bss section occupies space up to
        _end.  Align after .bss to ensure correct alignment even if the
-@@ -604,6 +628,7 @@ cat <<EOF
+@@ -605,6 +628,7 @@ cat <<EOF
        FIXME: Why do we need it? When there is no .bss section, we don't
        pad the .data section.  */
     ${RELOCATING+. = ALIGN(. != 0 ? ${ALIGNMENT} : 1);}
 +   ${RELOCATING+${CREATE_SHLIB-${RISCOS_ZILIMIT}}}
    }
    ${OTHER_BSS_SECTIONS}
-   ${RELOCATING+${OTHER_BSS_END_SYMBOLS}}
-@@ -613,6 +638,7 @@ cat <<EOF
+   ${LARGE_BSS_AFTER_BSS+${LARGE_BSS}}
+@@ -625,6 +649,7 @@ SHLIB_LARGE_DATA_ADDR=". = SEGMENT_START
    ${RELOCATING+${OTHER_END_SYMBOLS}}
    ${RELOCATING+${END_SYMBOLS-${USER_LABEL_PREFIX}_end = .; PROVIDE (${USER_LABEL_PREFIX}end = .);}}
    ${RELOCATING+${DATA_SEGMENT_END}}

gcc4/recipe/patches/gcc/gcc.doc.invoke.texi.p
@@ -1,6 +1,6 @@
 Index: gcc/doc/invoke.texi
 ===================================================================
---- gcc/doc/invoke.texi	(revision 197339)
+--- gcc/doc/invoke.texi	(revision 206264)
 +++ gcc/doc/invoke.texi	(working copy)
 @@ -472,6 +472,7 @@
  @gccoptlist{-mapcs-frame  -mno-apcs-frame @gol
@@ -10,7 +10,7 @@ Index: gcc/doc/invoke.texi
  -mapcs-float  -mno-apcs-float @gol
  -mapcs-reentrant  -mno-apcs-reentrant @gol
  -msched-prolog  -mno-sched-prolog @gol
-@@ -10548,7 +10549,8 @@
+@@ -10550,7 +10551,8 @@
  @item -mabi=@var{name}
  @opindex mabi
  Generate code for the specified ABI@.  Permissible values are: @samp{apcs-gnu},
@@ -20,7 +20,7 @@ Index: gcc/doc/invoke.texi
  
  @item -mapcs-frame
  @opindex mapcs-frame
-@@ -10562,8 +10564,6 @@
+@@ -10564,8 +10566,6 @@
  @opindex mapcs
  This is a synonym for @option{-mapcs-frame}.
  
@@ -29,7 +29,7 @@ Index: gcc/doc/invoke.texi
  @item -mapcs-stack-check
  @opindex mapcs-stack-check
  Generate code to check the amount of stack space available upon entry to
-@@ -10572,9 +10572,16 @@
+@@ -10574,9 +10574,16 @@
  @samp{__rt_stkovf_split_small} or @samp{__rt_stkovf_split_big} will be
  called, depending upon the amount of stack space required.  The runtime
  system is required to provide these functions.  The default is
@@ -48,7 +48,7 @@ Index: gcc/doc/invoke.texi
  @item -mapcs-float
  @opindex mapcs-float
  Pass floating-point arguments using the floating-point registers.  This is
-@@ -10584,6 +10591,7 @@
+@@ -10586,6 +10593,7 @@
  @option{-mno-apcs-float}, since integer only code is slightly increased in
  size if @option{-mapcs-float} is used.
  

gcc4/riscos/dist/!GCC/docs/index.html
@@ -748,7 +748,7 @@ This is the current ABI version and corresponds to V4.6 and onwards of GCC. Its 
 <p>This port of GCC is:
 <pre>
 Copyright (c) 1996, 1997, 1998, 1999, 2000, 2001 Nick Burrett
-Copyright (c) 2002-2013 GCCSDK Developers
+Copyright (c) 2002-2014 GCCSDK Developers
 </pre>
 
 <p>The GCCSDK releases made by the GCCSDK Developers consists of several binaries and sources which do not necessary have the same licenses.  The following overview can help to identify those licenses more easily but is in no way an authoritative statement on behalf of the GCCSDK Developers: